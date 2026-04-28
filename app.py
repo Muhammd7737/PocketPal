a9231a4bc7ec89b72a206eb7b44684d9decf75a6
Muhammad Shahid <shahidumar7737@gmail.com>
2026-04-28 14:08:55 -0600

    Increase password field length to 200 characters

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     #password = db.Column(db.String(100), nullable=False)
-    password  = db.Column(db.String(100), nullable=True)
+    password  = db.Column(db.String(200), nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=True)
     google_id = db.Column(db.String(128), unique=True, nullable=True)
     profile_pic = db.Column(db.Text, nullable=True) 
